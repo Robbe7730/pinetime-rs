f1fae7ab9529e2642f67b8e29edf5fee8de7c860
Robbe Van Herck <robbe@robbevanherck.be>
2022-01-27 19:38:45 +0100

    Cycle display brightness

--- src/display/mod.rs
+++ src/display/mod.rs
@@ -1,7 +1,17 @@
 use rtt_target::rprintln;
 
+use nrf52832_hal::prelude::OutputPin;
+
+use nrf52832_hal::gpio::Pin;
+use nrf52832_hal::gpio::Output;
+use nrf52832_hal::gpio::PushPull;
+
 pub struct Display {
     sleep: bool,
+    brightness: u8,
+    pin_backlight_low: Pin<Output<PushPull>>,
+    pin_backlight_mid: Pin<Output<PushPull>>,
+    pin_backlight_high: Pin<Output<PushPull>>,
 }
 
 #[repr(u8)]
@@ -22,9 +32,43 @@ impl Display {
         self.sleep = value;
     }
 
-    pub fn default() -> Display {
+    pub fn set_brightness(&mut self, value: u8) {
+        if (value & 0b001) == 0 {
+            self.pin_backlight_low.set_low().unwrap();
+        } else {
+            self.pin_backlight_low.set_high().unwrap();
+        }
+
+        if (value & 0b010) == 0 {
+            self.pin_backlight_mid.set_low().unwrap();
+        } else {
+            self.pin_backlight_mid.set_high().unwrap();
+        }
+
+        if (value & 0b100) == 0 {
+            self.pin_backlight_high.set_low().unwrap();
+        } else {
+            self.pin_backlight_high.set_high().unwrap();
+        }
+
+        self.brightness = value;
+    }
+
+    pub fn inc_brightness(&mut self) {
+        self.set_brightness(self.brightness + 1);
+    }
+
+    pub fn new(
+        pin_backlight_low: Pin<Output<PushPull>>,
+        pin_backlight_mid: Pin<Output<PushPull>>,
+        pin_backlight_high: Pin<Output<PushPull>>
+    ) -> Display {
         Display {
             sleep: true,
+            brightness: 0,
+            pin_backlight_low,
+            pin_backlight_mid,
+            pin_backlight_high,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,11 @@ mod pinetimers {
     use crate::display::Display;
 
     use rtt_target::{rprintln, rtt_init_print};
+
     use nrf52832_hal::pac::TIMER0;
+    use nrf52832_hal::gpio::Level;
+
+    use fugit::ExtU32;
 
     #[monotonic(binds = TIMER0, default = true)]
     type Mono0 = MonoTimer<TIMER0>;
@@ -34,10 +38,18 @@ mod pinetimers {
 
         display_init::spawn().unwrap();
 
+        let gpio = nrf52832_hal::gpio::p0::Parts::new(ctx.device.P0);
+
         let timer0: MonoTimer<TIMER0> = MonoTimer::new(ctx.device.TIMER0);
 
+        let display = Display::new(
+            gpio.p0_14.into_push_pull_output(Level::High).degrade(),
+            gpio.p0_22.into_push_pull_output(Level::High).degrade(),
+            gpio.p0_23.into_push_pull_output(Level::High).degrade()
+        );
+
         (Shared {
-            display: Display::default(),
+            display
         }, Local {}, init::Monotonics(timer0))
     }
     
@@ -53,7 +65,17 @@ mod pinetimers {
     fn display_init(mut ctx: display_init::Context) {
         ctx.shared.display.lock(|display| {
             display.set_sleep(false);
+            display.set_brightness(0x0);
         });
+        increase_brightness::spawn_after(1_u32.secs()).unwrap();
+    }
+
+    #[task(shared = [display])]
+    fn increase_brightness(mut ctx: increase_brightness::Context) {
+        ctx.shared.display.lock(|display| {
+            display.inc_brightness();
+        });
+        increase_brightness::spawn_after(1_u32.secs()).unwrap();
     }
 }
 
